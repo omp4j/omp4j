6211611d6e651c71155dade41864cee089fd3ccb
Petr Bělohlávek <me@petrbel.cz>
2014-06-19 08:37:47 +0200

    packages doc

---
+++ src/main/scala/Packages.scala
@@ -0,0 +1,9 @@
+package org {
+	/** omp4j main package. */
+	package object omp4j {}
+}
+
+package org.omp4j {
+	/** Classes used for preprocessor such as various listeners etc. */
+	package object preprocessor {}
+}
