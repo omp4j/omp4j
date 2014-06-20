569444ee0af2cba89a67abbbb58ee7124a500ada
Petr Bělohlávek <me@petrbel.cz>
2014-06-20 14:32:34 +0200

    org.omp4j.preprocessor.grammar doc

--- src/main/scala/Packages.scala
+++ src/main/scala/Packages.scala
@@ -7,3 +7,8 @@ package org.omp4j {
 	/** Classes used for preprocessor such as various listeners etc. */
 	package object preprocessor {}
 }
+
+package org.omp4j.preprocessor {
+	/** Auto-generated grammar classes by ANTLRv4 such as Parser, Lexer, etc. */
+	package object grammar {}
+}
