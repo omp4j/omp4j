3ba333c2d2d6824951775c4666b8deca82660c22
Petr Bělohlávek <me@petrbel.cz>
2014-06-24 10:34:45 +0200

    basic translator (comments only)

---
+++ src/main/scala/preprocessor/Translator.scala
@@ -0,0 +1,55 @@
+package org.omp4j.preprocessor
+
+import org.antlr.v4.runtime.atn._
+import org.antlr.v4.runtime.tree._
+import org.antlr.v4.runtime._
+
+import org.omp4j.preprocessor.grammar._
+import org.omp4j.preprocessor.exception._
+
+/** Translate context given with respect to directives */
+class Translator(directives: List[Directive], tokens: TokenStream, ctx: Java8Parser.CompilationUnitContext, tree: OMPFile) {
+
+	/** Get translated source code */
+	def translate(): String = {
+		val walker = new ParseTreeWalker()
+		val rl = new TranslationListener(directives, tokens, tree)
+		walker.walk(rl, ctx)
+		rl.rewriter.getText()
+	}
+
+	private class TranslationListener(directives: List[Directive], tokens: TokenStream, tree: OMPFile) extends Java8BaseListener {
+
+		/** Java8Parser.FieldDeclarationContext typedef */
+		type SC = Java8Parser.StatementContext
+		
+		/** Rewriter for directive expansions*/
+		var rewriter = new TokenStreamRewriter(tokens)
+
+		/** Filter statements with directive */
+		override def enterStatement(ctx: SC) = {
+			directives.find(d => d.ctx == ctx) match {
+				case Some(d) => addDirectiveHead(ctx)
+				case None => ;
+			}
+		}
+
+		/** Filter statements with directive */
+		override def exitStatement(ctx: SC) = {
+			directives.find(d => d.ctx == ctx) match {
+				case Some(d) => addDirectiveTail(ctx)
+				case None => ;
+			}
+		}
+
+		/** For now only add simple comment at the beginning -> insert context */
+		private def addDirectiveHead(ctx: SC) = {
+			rewriter.insertBefore(ctx.start, "/* start */ ");
+		}
+
+		/** For now only add simple comment at the end */
+		private def addDirectiveTail(ctx: SC) = {
+			rewriter.insertAfter(ctx.stop, " /* end */");
+		}
+	}
+}
