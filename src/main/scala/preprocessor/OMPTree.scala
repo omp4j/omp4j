c990dd01018f22b143754519702c5e1a30e77926
Petr Bělohlávek <me@petrbel.cz>
2014-06-24 10:38:14 +0200

    imports + doc

--- src/main/scala/preprocessor/OMPTree.scala
+++ src/main/scala/preprocessor/OMPTree.scala
@@ -1,14 +1,9 @@
 package org.omp4j.preprocessor
 
-import org.antlr.v4.runtime.ANTLRFileStream;
-import org.antlr.v4.runtime.BailErrorStrategy;
-import org.antlr.v4.runtime.CommonTokenStream;
-import org.antlr.v4.runtime.DiagnosticErrorListener;
-import org.antlr.v4.runtime.Lexer;
-import org.antlr.v4.runtime.ParserRuleContext;
-import org.antlr.v4.runtime.atn.LexerATNSimulator;
-import org.antlr.v4.runtime.atn.PredictionMode;
+import org.antlr.v4.runtime.atn._
 import org.antlr.v4.runtime.tree._
+import org.antlr.v4.runtime._
+
 import scala.collection.mutable.ListBuffer
 
 import org.omp4j.preprocessor.grammar._
@@ -23,6 +18,7 @@ class OMPFile(ctx: Java8Parser.CompilationUnitContext, parser: Java8Parser) {
 	override def toString() = ctx.toStringTree(parser)
 }
 
+/** Class representation containing list of methods, fields and nested classes */
 class OMPClass(ctx: Java8Parser.ClassDeclarationContext, parser: Java8Parser) {
 	val nestedClasses = (new ClassExtractor ).visit(ctx.classBody()).map(c => new OMPClass(c, parser))
 	val methods = (new MethodExtractor ).visit(ctx.classBody()).map(c => new OMPMethod(c, parser))
@@ -35,6 +31,7 @@ class OMPClass(ctx: Java8Parser.ClassDeclarationContext, parser: Java8Parser) {
 
 
 // TODO: constructor
+/** Method representation */
 class OMPMethod(ctx: Java8Parser.MethodDeclarationContext, parser: Java8Parser) {
 	// val tree: OMPTree
 	// val variables: List[OMPVariable] // TODO
@@ -44,5 +41,6 @@ class OMPMethod(ctx: Java8Parser.MethodDeclarationContext, parser: Java8Parser) 
 
 class OMPTree() {}
 
+/** Variable representation */
 class OMPVariable(varType: Java8Parser.TypeContext, ctx: Java8Parser.VariableDeclaratorsContext, parser: Java8Parser) {
 }
