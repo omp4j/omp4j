bef56ba800505c6e4b38c2e6c6b80ff5cc81802c
Petr Bělohlávek <me@petrbel.cz>
2014-06-25 13:44:08 +0200

    SyntaxErrorException

--- src/main/scala/exception/Exceptions.scala
+++ src/main/scala/exception/Exceptions.scala
@@ -1,3 +1,4 @@
 package org.omp4j.preprocessor.exception
 
 class ParseException(message: String = null, cause: Throwable = null) extends RuntimeException(message, cause)
+class SyntaxErrorException(message: String = null, cause: Throwable = null) extends RuntimeException(message, cause)
