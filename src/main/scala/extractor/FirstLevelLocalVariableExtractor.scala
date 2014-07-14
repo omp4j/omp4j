df20d3f6512c0cc91cb4b57937d898b5cf7a02b1
Petr Bělohlávek <me@petrbel.cz>
2014-07-14 09:15:12 +0200

    using OMPVariables

--- src/main/scala/extractor/FirstLevelLocalVariableExtractor.scala
+++ src/main/scala/extractor/FirstLevelLocalVariableExtractor.scala
@@ -5,22 +5,19 @@ import org.antlr.v4.runtime.tree._
 import org.antlr.v4.runtime._
 
 import org.omp4j.preprocessor.grammar._
+import org.omp4j.preprocessor._
 
 /** Extracts all local variable declarations from ANTLR4 ParseTree with no statement pass */
-class FirstLevelLocalVariableExtractor extends Java8BaseVisitor[Set[Java8Parser.LocalVariableDeclarationContext]] {
-
-	/** Java8Parser.LocalVariableDeclarationContext typedef */
-	type LVDC = Java8Parser.LocalVariableDeclarationContext
+class FirstLevelLocalVariableExtractor extends Java8BaseVisitor[Set[OMPVariable]] {
 
 	/** Add local variable declaration context */
-	override def visitLocalVariableDeclaration(variableCtx: LVDC) = Set[LVDC](variableCtx)
+	override def visitLocalVariableDeclaration(variableCtx: Java8Parser.LocalVariableDeclarationContext) = Set[OMPVariable](new OMPVariable(variableCtx.variableDeclarators().variableDeclarator(0).variableDeclaratorId().getText(), variableCtx.`type`().getText()))
 
 	/** Don't get into nested statements */
-	override def visitStatement(c: Java8Parser.StatementContext) = Set[LVDC]()
+	override def visitStatement(ctx: Java8Parser.StatementContext) = Set[OMPVariable]()
 
 	/** Don't get into other methods */
-	override def visitMethodDeclaration(c: Java8Parser.MethodDeclarationContext) = Set[LVDC]()
-
-	override def defaultResult() = Set[LVDC]()
-	override def aggregateResult(a: Set[LVDC], b: Set[LVDC]) = a ++ b
+	override def visitMethodDeclaration(ctx: Java8Parser.MethodDeclarationContext) = Set[OMPVariable]()
+	override def defaultResult() = Set[OMPVariable]()
+	override def aggregateResult(a: Set[OMPVariable], b: Set[OMPVariable]) = a ++ b
 }
