e16671f4a50112f142ab97546b960f0e024cd131
Petr Bělohlávek <me@petrbel.cz>
2014-07-14 09:15:39 +0200

    token storing

--- src/main/scala/preprocessor/Directive.scala
+++ src/main/scala/preprocessor/Directive.scala
@@ -1,9 +1,11 @@
 package org.omp4j.preprocessor
 
 import org.omp4j.preprocessor.grammar._
+import org.antlr.v4.runtime.Token
 
 /** Directive structure */
 case class Directive(
+	cmt:       Token,
 	ompCtx:    OMPParser.OmpUnitContext,
 	ompParser: OMPParser,
 	ctx:       Java8Parser.StatementContext,
