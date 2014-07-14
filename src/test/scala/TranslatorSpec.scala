39dc05ad53f439116ab760c5f3c0e949ac3d13ec
Petr Bělohlávek <me@petrbel.cz>
2014-07-14 09:22:33 +0200

    testing inherited params

--- src/test/scala/TranslatorSpec.scala
+++ src/test/scala/TranslatorSpec.scala
@@ -14,20 +14,30 @@ import org.omp4j.preprocessor.grammar._
 class TranslatorLoadedContext(path: String) extends AbstractLoadedContext(path) {
 
 	/** Return set of string <type> <identifier> such as "int ok1" etc.*/
-	def tranLinAsText = {
+	def localsAsText = {
 		val inherLocals = (new Translator(tokens, parser, directives, null)(null)).getPossiblyInheritedLocals(directives.head.ctx)
-		inherLocals.map{ l => l.`type`().getText() + " " + l.variableDeclarators().variableDeclarator(0).variableDeclaratorId().getText() }
+		inherLocals.map{ v => v.varType + " " + v.name }
 	}
+
+	/** Return set of string <type> <identifier> such as "int ok1" etc.*/
+	def paramsAsText = {
+		val inherLocals = (new Translator(tokens, parser, directives, null)(null)).getPossiblyInheritedParams(directives.head.ctx)
+		inherLocals.map{ v => v.varType + " " + v.name }
+	}
+
 }
 
 /** Unit test for Translator */
 class TranslatorSpec extends AbstractSpec {
 
 	// check inherited vars in block after first (and the only) directive
-	(new TranslatorLoadedContext("/inheritedLocals/01.java")).tranLinAsText should contain only ("int ok1", "int ok2", "int ok3")
-	(new TranslatorLoadedContext("/inheritedLocals/02.java")).tranLinAsText should contain only ("int ok1", "int ok2", "int ok3", "String ok4", "int ok5", "float ok6", "int ok7")
-	(new TranslatorLoadedContext("/inheritedLocals/03.java")).tranLinAsText should contain only ("int ok1", "int ok2")
-	(new TranslatorLoadedContext("/inheritedLocals/04.java")).tranLinAsText should contain only ("int ok1", "int ok2")
-	(new TranslatorLoadedContext("/inheritedLocals/05.java")).tranLinAsText should contain only ("int ok1", "int ok2")
+	(new TranslatorLoadedContext("/inheritedLocals/01.java")).localsAsText should contain only ("int ok1", "int ok2", "int ok3")
+	(new TranslatorLoadedContext("/inheritedLocals/02.java")).localsAsText should contain only ("int ok1", "int ok2", "int ok3", "String ok4", "int ok5", "float ok6", "int ok7")
+	(new TranslatorLoadedContext("/inheritedLocals/03.java")).localsAsText should contain only ("int ok1", "int ok2")
+	(new TranslatorLoadedContext("/inheritedLocals/04.java")).localsAsText should contain only ("int ok1", "int ok2")
+	(new TranslatorLoadedContext("/inheritedLocals/05.java")).localsAsText should contain only ("int ok1", "int ok2")
+
+	// check method params
+	(new TranslatorLoadedContext("/inheritedParams/01.java")).paramsAsText should contain only ("String[] args", "int a", "String b", "float c")
 
 }
