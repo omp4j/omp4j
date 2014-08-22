390f18c6eda6185763ec6c45aad8546818b7bb16
Petr Bělohlávek <me@petrbel.cz>
2014-08-22 09:46:38 +0200

    multiple local var. declaration in a single statement

--- src/main/scala/extractor/FirstLevelLocalVariableExtractor.scala
+++ src/main/scala/extractor/FirstLevelLocalVariableExtractor.scala
@@ -7,11 +7,25 @@ import org.antlr.v4.runtime._
 import org.omp4j.preprocessor.grammar._
 import org.omp4j.preprocessor._
 
+import scala.collection.JavaConverters._
+
 /** Extracts all local variable declarations from ANTLR4 ParseTree with no statement pass */
 class FirstLevelLocalVariableExtractor extends Java8BaseVisitor[Set[OMPVariable]] {
 
 	/** Add local variable declaration context */
-	override def visitLocalVariableDeclaration(variableCtx: Java8Parser.LocalVariableDeclarationContext) = Set[OMPVariable](new OMPVariable(variableCtx.variableDeclarators().variableDeclarator(0).variableDeclaratorId().getText(), variableCtx.`type`().getText()))
+	override def visitLocalVariableDeclaration(variableCtx: Java8Parser.LocalVariableDeclarationContext) = {
+		var result = Set[OMPVariable]()
+		val varList = variableCtx.variableDeclaratorList.variableDeclarator
+		val varType = variableCtx.unannType.getText
+		varList.asScala.foreach{v =>
+			result += new OMPVariable(
+				v.variableDeclaratorId.getText,
+				varType,
+				OMPVariableType.Local
+			)
+		}
+		result
+	}
 
 	/** Don't get into nested statements */
 	override def visitStatement(ctx: Java8Parser.StatementContext) = Set[OMPVariable]()
