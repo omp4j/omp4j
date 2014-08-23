35a272dde092790bc596d41442ebb913f43bbb20
Petr Bělohlávek <me@petrbel.cz>
2014-08-23 17:08:48 +0200

    using ANTLRv4.4

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 # download ANTLRv4 runtime dependency
-curl -o lib/antlr-runtime-4.2.2.jar http://www.antlr.org/download/antlr-runtime-4.2.2.jar
+curl -o lib/antlr-runtime-4.4.jar http://www.antlr.org/download/antlr-runtime-4.4.jar
 
 cd src/main/java/grammar
-curl -O http://www.antlr.org/download/antlr-4.2-complete.jar
+curl -O http://www.antlr.org/download/antlr-4.4-complete.jar
 
-java -jar antlr-4.2-complete.jar -visitor OMP.g4
-java -jar antlr-4.2-complete.jar -visitor Java8.g4
+java -jar antlr-4.4-complete.jar -visitor OMP.g4
+java -jar antlr-4.4-complete.jar -visitor Java8.g4
