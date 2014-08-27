232bd46c8693311608d927a71afc5b5371cc75d1
Petr Bělohlávek <me@petrbel.cz>
2014-08-27 09:38:02 +0200

    complex for example

--- example/02_for.java
+++ example/02_for.java
@@ -1,7 +1,7 @@
 class For {
 	public static void main(String[] args) {
 		// omp parallel for
-		for (int i = 3; i < 13; i+=1) {
+		for (int i = 0; i < 20; i += 3) {
 			System.out.println("hello @" + i);
 		}
 	}
